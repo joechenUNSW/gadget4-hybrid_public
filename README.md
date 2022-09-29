b802eb518c1a6a20bb0cff095b06a3c682876964
MarkMos <markus.mosbech@gmail.com>
2022-09-29 11:04:12 +1000

    Added restart prep files, updated readme.

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 
 
-GADGET-4
+GADGET-4-hybrid
+========
+
+Standard GADGET-4
 ========
 
 ![](documentation/img/top.jpg)
@@ -19,11 +22,16 @@ including [Ruediger Pakmor](mailto:rpakmor@mpa-garching.mpg.de),
 [Oliver Zier](mailto:ozier@mpa-garching.mpg.de), and
 [Martin Reinecke](mailto:martin@mpa-garching.mpg.de).
 
-
-Documentation
-=============
-
 For documentation of the code as well as the code paper, please refer
 to the [code's web-site](https://wwwmpa.mpa-garching.mpg.de/gadget4).
 
 
+Neutrinos
+=============
+Hybrid neutrinos are implemented by Joe Zhiyu Chen. Linear response neutrinos implemented as described in papers [arXiv:2011.12503](https://arxiv.org/abs/2011.12503) and [arXiv:2011.12504](https://arxiv.org/abs/2011.12504). Paper to describe the hybrid implementation is in preparation.
+
+To generate accurate initial conditions for neutrino linear response, a scaled-back input power spectrum taking neutrinos into account is necessary. This can be obtained with the [MuFLR code](https://github.com/upadhye/MuFLR).
+
+Input files for restarting can be prepared easily with the restart\_preparation.py (when converting a single stream) and restart\_preparation\_multi.py (when converting multiple streams) scripts. Scripts prepared by Markus Mosbech.
+
+Fully updated documentation for the changes to base Gadget parameters is in the works.
---
+++ restart_preparation.py
@@ -0,0 +1,107 @@
+import sys#, getopt
+import numpy as np
+#import matplotlib.pyplot as plt
+#import h5py
+import argparse
+from pathlib import Path
+
+
+
+def main():
+
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o','--output_path',nargs='?',default='')
+    parser.add_argument('-d','--input_delta_nu',nargs='?')
+    parser.add_argument('-t','--input_theta_nu',nargs='?')
+    parser.add_argument('-p','--input_pcb',nargs='?')
+    parser.add_argument('-n','--streamnumber', type=int, nargs='?',default=1)
+    parser.add_argument('-N','--Nstreams', type=int, nargs='?',default=10)
+    parser.add_argument('-v','--verbose',action='store_true')
+    args = parser.parse_args()
+
+    #print(args)
+
+    outputpath = args.output_path
+    Path(outputpath).mkdir(parents=True,exist_ok=True)
+
+    if args.verbose:
+        myprint=pleaseprint
+    else:
+        myprint=dontprint
+
+    if outputpath !='':
+        if outputpath[-1]!='/':
+            outputpath = outputpath + '/'
+
+
+
+    streamno = args.streamnumber
+
+    Nstreams = args.Nstreams
+
+    if (hasattr(args,'input_delta_nu') and args.input_delta_nu != None):
+        myprint('Processing delta_nu')
+        delta_nu_dat = np.loadtxt(args.input_delta_nu,delimiter=',',usecols=range(0,Nstreams+1))
+        k_delta = delta_nu_dat[:,0]
+        delta = delta_nu_dat[:,streamno]**2
+        data_delta = np.array([delta,delta])
+        np.savetxt(outputpath+'delta_nu.txt',data_delta.transpose())
+        myprint('delta_nu done')
+
+    if (hasattr(args,'input_theta_nu') and args.input_theta_nu != None):
+        if hasattr(args,'input_delta_nu'):
+            myprint('Processing growthrate')
+            theta_nu_dat = np.loadtxt(args.input_theta_nu,delimiter=',',usecols=range(0,Nstreams+1))
+            growthrate = theta_nu_dat[:,streamno]/delta_nu_dat[:,streamno]
+            data_growthrate = np.array([delta_nu_dat[:,0],growthrate])
+            np.savetxt(outputpath+'growthrate.txt',data_growthrate.transpose())
+            myprint('growthrate done')
+        else:
+            raise UserWarning('Cannot calculate growthrate without delta')
+
+
+
+
+    if (hasattr(args,'input_pcb')and args.input_pcb != None):
+        myprint('Processing P_cb')
+        Pcbdat = load_powerspec(args.input_pcb)
+        np.savetxt(outputpath+'Pcb.txt',Pcbdat)
+        myprint('P_cb done')
+
+
+
+
+
+
+
+def load_powerspec(powerspecfile):
+    header_length = 5
+    with open(powerspecfile) as file:
+        lines = file.readlines()
+        lines = [line.rstrip() for line in lines]
+    numlines = int(lines[1])
+    boxlength = float(lines[2])
+
+    datalines = lines[header_length:header_length+numlines]
+
+    data = np.zeros([numlines,5])
+
+    for i, line in enumerate(datalines):
+        data[i,:] = np.fromstring(line, dtype=float, sep=' ')
+
+    k = data[:,0]
+    pk = data[:,2]*boxlength**3 
+
+    datarray = np.array([k,pk])
+
+    return datarray.transpose()
+
+def pleaseprint(str):
+    print(str)
+
+def dontprint(str):
+    pass
+
+if __name__ == "__main__":
+   main()
